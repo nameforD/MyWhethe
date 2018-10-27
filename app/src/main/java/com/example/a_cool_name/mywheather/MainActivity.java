50f96936571f58077982b1d5e00cb4b47752ec2f
Cool_name <Cool_name@gmail.com>
2018-10-27 16:04:57 +0800

    First commit

---
+++ app/src/main/java/com/example/a_cool_name/mywheather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a_cool_name.mywheather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
